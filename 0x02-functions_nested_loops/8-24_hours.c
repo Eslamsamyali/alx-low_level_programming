a1f8676a1852c46075cba30b15fc627a6764e9c4
eslam <essamyali2@gmail.com>
2023-08-15 10:23:51 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,13 +2,16 @@
 
 /**
  * jack_bauer- prints every minute of the day
+ *
+ * Description: prints every minute of the day
+ * of Jack Bauer, starting from 00:00 to 23:59.
 */
 
 void jack_bauer(void)
 {
 	int min, hr;
 
-	for (min = 0; hr <= 23; hr++)
+	for (hr = 0; hr <= 23; hr++)
 	{
 		for (min = 0; min <= 59; min++)
 		{
