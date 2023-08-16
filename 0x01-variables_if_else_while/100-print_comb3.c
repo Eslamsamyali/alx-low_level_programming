326aeb16b1cd6ef69865d54b048a4b6fc730c1c9
eslam <essamyali2@gmail.com>
2023-08-16 11:22:07 -0700

    0-main.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,28 +10,27 @@
 */
 int main(void)
 {
-	int dight1 = 0, dight2;
+        int dight1 = 0, dight2;
 
-	while (dight2 <= 9)
-	{
-		dight2 = 0;
-		while (dight2 <= 9)
-		{
-			if (dight1 != dight2 && dight1 < dight2)
-			{
-				putchar(dight1 + 48);
-				putchar(dight2 + 48);
-				if (dight1 + dight2 != 17)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		dight2++;
-		}
-	}
-	dight1++;
-	putchar('\n');
-
-	return (0);
+        while (dight1 <= 9)
+        {
+                dight2 = dight1 + 1;
+                while (dight2 <= 9)
+                {
+                        if (dight1 != dight2 && dight1 < dight2)
+                        {
+                                putchar(dight1 + 48);
+                                putchar(dight2 + 48);
+                                if (dight1 + dight2 != 17)
+                                {
+                                        putchar(',');
+                                        putchar(' ');
+                                }
+                        }
+                        dight2++;
+                }
+                dight1++;
+        }
+        putchar('\n');
+        return (0);
 }
