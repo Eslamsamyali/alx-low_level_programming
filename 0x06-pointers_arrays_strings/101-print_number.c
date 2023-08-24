bf934ff41bef0f1a1f48fac68391abd4fc717c64
eslam <essamyali2@gmail.com>
2023-08-23 19:19:14 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the integer to print
+ *
+ * Return: void
+*/
+void print_number(int n)
+{
+	int i, j, div;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	div = 1;
+	i = n;
+	while (i > 9)
+	{
+		i /= 10;
+		div *= 10;
+	}
+	while (div >= 1)
+	{
+		j = n / div;
+		_putchar(j + '0');
+		n -= j * div;
+		div /= 10;
+	}
+	return (s);
+}
