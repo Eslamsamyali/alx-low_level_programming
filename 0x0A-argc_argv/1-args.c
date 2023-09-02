93a5d0216dc103624a1fc651ca2a7c1043009bae
eslam <essamyali2@gmail.com>
2023-09-01 19:23:12 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,8 +8,12 @@
  */
 int main(int argc, char *argv[])
 {
-	/* print the number of arguments minus one */
-	printf("%d\n", argc - 1);
+	/* declare a variable to store the number of user arguments */
+	int user_args;
+	/* subtract one from argc to get the number of user arguments */
+	user_args = argc - 1;
+	/* print the number of user arguments followed by a new line */
+	printf("%d\n", user_args);
 	/* return 0 to indicate success */
 	return (0);
 }
