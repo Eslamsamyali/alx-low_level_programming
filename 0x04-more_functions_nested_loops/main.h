8ae86c6f7a1d6456cb3b0bb6d369ee7b7a622641
eslam <essamyali2@gmail.com>
2023-09-01 17:14:49 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,13 @@
 #define MAIN_H
 
 /* Function prototypes and other declarations here */
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+
 
 #endif /* MAIN_H */
 
