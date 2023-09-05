11c3d66548efb88fb6b1c35490aa8395bcb6a089
eslam <essamyali2@gmail.com>
2023-09-05 14:09:20 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,44 +1,100 @@
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * strtow - Splits a string into words
- * @str: The string to split
- *
- * Return: A pointer to an array of strings (words)
- */
+* count_words - Count the number of words on a given string.
+* @str: string to count words.
+* Return: Amount of words.
+*/
+int count_words(char *str)
+{
+	int wc = 0, word = 0;
+
+	for (; *str; str++)
+		if (*str != ' ' && !word)
+		{
+			wc++;
+			word = 1;
+		}
+		else if (*str == ' ')
+		{
+			word = 0;
+		}
+	return (wc);
+}
+/**
+* alloc_array - Creates array with words size.
+* @tmp: String to evaluate.
+* @arr: Array defined to store the result.
+* Return: Array ready to be filled.
+*/
+char **alloc_array(char *tmp, char ***arr)
+{
+	int wc = 0, word = 0, i = 0;
+
+	for (; *tmp; tmp++)
+	{
+		if (*tmp != ' ')
+		{
+			wc++;
+			word = 1;
+		}
+		if ((*tmp == ' ' || !*(tmp + 1)) && word)
+		{
+			word = 0;
+			*(*arr + i) = malloc(wc + 1);
+			wc = 0;
+			if (!*(*arr + i))
+			{
+				while (i)
+				{
+					free(*(*arr + i));
+					i--;
+				}
+				free(*arr);
+				free(arr);
+				return (0);
+			}
+			i++;
+		}
+	}
+	return (*arr);
+}
+/**
+* strtow - Splits a string into words.
+* @str: String to split.
+* Return: Vector of words, or NULL if failed..
+*/
 char **strtow(char *str)
 {
-	char **tab;
-	int i, j, k, l, count = 0;
+	int wc = 0, word = 0, i = 0, j = 0;
+	char **arr;
 
-	if (str == NULL || str[0] == '\0')
-		return (NULL);
+	if (!str || *str == '\0')
+		return (0);
+	wc = count_words(str);
+	if (!wc)
+		return (0);
+	arr = malloc((wc + 1) * 8);
+	if (!arr)
+		return (0);
+	arr[wc] = NULL;
 
-	for (i = 0; str[i]; i++)
-		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
-			count++;
-	tab = malloc(sizeof(char *) * (count + 1));
-	if (tab == NULL)
-		return (NULL);
-	tab[count] = NULL;
-	for (i = 0, l = 0; str[i]; i++)
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+	arr = alloc_array(str, &arr);
+	if (!arr)
+	{
+		free(arr);
+		return (0);
+	}
+	for (; *str; str++)
+		if (*str != ' ')
 		{
-			for (j = i; str[j] && str[j] != ' '; j++)
-				;
-			tab[l] = malloc(j - i + 1);
-			if (tab[l] == NULL)
-			{
-				for (k = 0; k < l; k++)
-					free(tab[k]);
-				free(tab);
-				return (NULL);
-			}
-			for (k = 0; k < j - i; k++)
-				tab[l][k] = str[i + k];
-			tab[l][k] = '\0';
-			l++;
+			arr[i][j++] = *str;
+			word = 1;
 		}
-	return (tab);
+		else if (*str == ' ' && word)
+		{
+			word = 0;
+			arr[i++][j] = '\0';
+			j = 0;
+		}
+	return (arr);
 }
