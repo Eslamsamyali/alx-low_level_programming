dbf2a5c2abd371e23f1a8377e9bb89359763b333
eslam <essamyali2@gmail.com>
2023-09-07 19:37:44 -0700

    2-calloc.c2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,37 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * _calloc - allocates memory for an array, using malloc
- * @nmemb: number of elements in the array
- * @size: size of each element in bytes
- * Return: pointer to the allocated memory
- * The memory is set to zero
- * If nmemb or size is 0, then _calloc returns NULL
- * If malloc fails, then _calloc returns NULL
+ * _calloc - Allocates memory for an array
+ * @nmemb: Number of elements in the array
+ * @size: Size of each element
+ * Return: A pointer to the allocated memory, or NULL on failure
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p; /* pointer to the allocated memory */
-	unsigned int i, total; /* loop and total size variables */
+	void *ptr;
+	unsigned int i;
 
-	/* if nmemb or size is 0, return NULL */
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	/* calculate the total size of memory needed */
-	total = nmemb * size;
 
-	/* allocate memory using malloc */
-	p = malloc(total);
+	ptr = malloc(nmemb * size);
 
-	/* check if malloc failed */
-	if (p == NULL)
+	if (ptr == NULL)
 		return (NULL);
 
-	/* set the memory to zero using memset */
-	memset(p, 0, total);
+	for (i = 0; i < nmemb * size; i++)
+		*((char *)ptr + i) = 0;
 
-	/* return the pointer to the allocated memory */
-	return (p);
+	return (ptr);
 }
