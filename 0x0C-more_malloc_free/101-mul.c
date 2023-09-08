ce9aaf91ce550f4d03d9341e08b32662a316c466
eslam <essamyali2@gmail.com>
2023-09-07 18:56:46 -0700

    101-mul.c3

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,65 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- * _isdigit - checks if a string is a digit
- * @s: string to check
- *
- * Return: 1 if s is a digit, 0 otherwise
- */
-int _isdigit(char *s)
-{
-	while (*s)
-	{
-		if (*s < '0' || *s > '9')
-			return (0);
-		s++;
-	}
-	return (1);
-}
-
 /**
  * main - multiplies two positive numbers
- * @argc: number of arguments passed to program
- * @argv: array of arguments passed to program
+ * @argc: number of command-line arguments
+ * @argv: array of command-line arguments
  *
- * Return: 0 on success, 98 on failure
- */
-int main(int argc, char **argv)
+ * Description: This program takes two command-line arguments,
+ * converts them to integers, and then multiplies them.
+ * The result is printed to the standard output.
+ * If the number of arguments is incorrect or if either of the
+ * arguments is not a positive integer, the program prints an error
+ * message and exits with a status of 98.
+ *
+ * Return: 0 if successful, 98 otherwise
+*/
+int main(int argc, char *argv[])
 {
-	int i, j, len1 = 0, len2 = 0, lenres;
-	int *res;
-	char *s1, *s2;
+	int num1, num2, product;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
 
-	s1 = argv[1], s2 = argv[2];
-	if (!_isdigit(s1) || !_isdigit(s2))
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	if (num1 <= 0 || num2 <= 0)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
-	while (s1[len1 + 1])
-		len1++;
-	while (s2[len2 + 1])
-		len2++;
-	lenres = len1 + len2;
-	res = calloc(lenres + 1, sizeof(int));
-	if (!res)
-		return (98);
-	for (i = len1; i >= 0; i--)
-		for (j = len2; j >= 0; j--)
-			res[i + j] += (s1[i] - '0') * (s2[j] - '0');
-	for (i = lenres; i > 0; i--)
-		if (res[i] > 9)
-			res[i - 1] += res[i] / 10, res[i] %= 10;
-	for (i = !res[0]; i <= lenres; i++)
-		printf("%d", res[i]);
-	printf("\n");
-	free(res);
+
+	product = num1 * num2;
+	printf("%d\n", product);
+
 	return (0);
 }
