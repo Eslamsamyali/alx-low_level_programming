e38742fd0645232afea741064427d9b3904b383a
eslam <essamyali2@gmail.com>
2023-10-02 15:57:32 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,54 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: the name of the file to read
- * @letters: the number of letters to read and print
- * Return: the actual number of letters read and printed, or 0 if error
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd; /* file descriptor */
-char *buf; /* buffer to store the file content */
-ssize_t n_read, n_write; /* number of bytes read and written */
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-/* check if filename is NULL */
-if (filename == NULL)
-return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-/* open the file for reading only */
-fd = open(filename, O_RDONLY);
-/* check if open failed */
-if (fd == -1)
-return (0);
-
-/* allocate memory for the buffer */
-buf = malloc(sizeof(char) * letters);
-/* check if malloc failed */
-if (buf == NULL)
-return (0);
-
-/* read from the file to the buffer */
-n_read = read(fd, buf, letters);
-/* check if read failed */
-if (n_read == -1)
-return (0);
-
-/* close the file */
-close(fd);
-
-/* write from the buffer to the standard output */
-n_write = write(STDOUT_FILENO, buf, n_read);
-/* check if write failed or did not write the expected amount of bytes */
-if (n_write == -1 || n_write != n_read)
-return (0);
-
-/* free the buffer */
-free(buf);
-
-/* return the number of bytes written */
-return (n_write);
+	free(buf);
+	close(fd);
+	return (w);
 }
